b2b5f450959f9c833deb4a6dab3f3b8139db7ef4
ludak <ludak@ludak.me>
2016-08-01 00:59:58 +0000

    Resize story overlay on tilt in mobile devices

--- www/public/js/helpers.js
+++ www/public/js/helpers.js
@@ -1,7 +1,7 @@
 function openStoryImageOverlay(source)
 {
-  var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  var h = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  var w = $(window).width();
+  var h = $(window).height();
 
   var overlay = document.getElementById("story-image-overlay-div")
   overlay.style.display = "block";
@@ -23,4 +23,12 @@ $(function() {
   var mastheadElem = document.querySelector('#masthead');
   var mastheadHeadroom  = new Headroom(mastheadElem);
   mastheadHeadroom.init();
+});
+
+/*reload story image overlay on resize*/
+$(window).resize(function() {
+  if ($("#story-image-overlay-div").is(":visible"))
+  {
+    openStoryImageOverlay($("#story-image-overlay-img").attr("src"));
+  }
 });
