399dc3d04a44f620fe7474e82656c3b61c937f76
ludak <ludak@ludak.me>
2016-08-01 04:55:06 +0000

    Add caption to overlay image

--- www/public/js/helpers.js
+++ www/public/js/helpers.js
@@ -1,24 +1,19 @@
 var storyImageOverlayImg = '#story-image-overlay-img'
 var storyImageOverlayDiv = '#story-image-overlay-div'
+var storyImageOverlayCaption = '#story-image-overlay-caption'
+var storyImageOverlayContentDiv = '#story-image-overlay-content-div'
 
 /*open story image overlay*/
 function openStoryImageOverlay(source, caption)
 {
-  var w = $(window).width();
-  var h = $(window).height();
-
   $(storyImageOverlayDiv).css("display","block");
 
-  $(storyImageOverlayImg).attr('src', source)
-  $(storyImageOverlayImg).attr('alt', caption)
-  $(storyImageOverlayImg).css("maxWidth", w);
-  $(storyImageOverlayImg).css("maxHeight", h);
-}
+  $(storyImageOverlayImg).attr('src', source);
+  $(storyImageOverlayImg).attr('alt', caption);
+  $(storyImageOverlayImg).css("maxWidth", $(window).width());
+  $(storyImageOverlayImg).css("maxHeight", $(window).height());
 
-/*close story image overlay*/
-function closeStoryImageOverlay()
-{
-  $(storyImageOverlayDiv).css("display","none");
+  $(storyImageOverlayCaption).html(caption);
 }
 
 /*hide masthead on scroll down.*/
@@ -29,9 +24,31 @@ $(function() {
 });
 
 /*reload story image overlay on resize*/
-$(window).resize(function() {
-  if ($(storyImageOverlayDiv).is(":visible"))
+$(window).resize(function()
   {
-    openStoryImageOverlay($(storyImageOverlayImg).attr("src"));
-  }
-});
+    if ($(storyImageOverlayDiv).is(":visible"))
+    {
+      openStoryImageOverlay($(storyImageOverlayImg).attr("src"));
+    }
+  });
+
+$(function ()
+  {
+    /*onClick handler for story image overlay image*/
+    $(storyImageOverlayImg).on('click', function()
+      {
+        $(storyImageOverlayDiv).css("display","none");
+      });
+
+    /*update story image overlay caption on image load*/
+    $(storyImageOverlayImg).on('load', function()
+      {
+        var imgOffset = $(storyImageOverlayImg).offset();
+
+        $(storyImageOverlayCaption).css('left', imgOffset.left);
+        $(storyImageOverlayCaption).css("width", $(storyImageOverlayImg).width());
+
+        $(storyImageOverlayContentDiv).css("width", $(window).width());
+        $(storyImageOverlayContentDiv).css("height", $(window).height());
+      });
+  });
