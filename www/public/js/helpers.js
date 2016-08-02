10991bdc6c2bcd19b8e77c6d1adfff5fafe7958c
ludak <ludak@ludak.me>
2016-08-02 05:50:51 +0000

    Align overlay image caption to the bottom left of overlay image

--- www/public/js/helpers.js
+++ www/public/js/helpers.js
@@ -8,12 +8,27 @@ function openStoryImageOverlay(source, caption)
 {
   $(storyImageOverlayDiv).css("display","block");
 
+  $(storyImageOverlayContentDiv).css('width', $(window).width());
+  $(storyImageOverlayContentDiv).css('height', $(window).height());
+
   $(storyImageOverlayImg).attr('src', source);
   $(storyImageOverlayImg).attr('alt', caption);
   $(storyImageOverlayImg).css("maxWidth", $(window).width());
   $(storyImageOverlayImg).css("maxHeight", $(window).height());
 
   $(storyImageOverlayCaption).html(caption);
+}
+
+/*reload story image overlay caption*/
+function reloadStoryImageOverlayCaption()
+{
+  var captionDivLeftPos = $(storyImageOverlayImg).css('margin-left');
+  var captionDivBottomPos = $(storyImageOverlayImg).css('margin-bottom');
+
+
+  $(storyImageOverlayCaption).css('left', captionDivLeftPos);
+  $(storyImageOverlayCaption).css('bottom', captionDivBottomPos);
+  $(storyImageOverlayCaption).css('width', $(storyImageOverlayImg).width());
 }
 
 /*hide masthead on scroll down.*/
@@ -28,7 +43,8 @@ $(window).resize(function()
   {
     if ($(storyImageOverlayDiv).is(":visible"))
     {
-      openStoryImageOverlay($(storyImageOverlayImg).attr("src"));
+      openStoryImageOverlay($(storyImageOverlayImg).attr("src"), $(storyImageOverlayCaption).html());
+      reloadStoryImageOverlayCaption();
     }
   });
 
@@ -40,15 +56,24 @@ $(function ()
         $(storyImageOverlayDiv).css("display","none");
       });
 
+    /*onClick handler for story image overlay caption*/
+    $(storyImageOverlayCaption).on('click', function()
+      {
+        if ($(storyImageOverlayImg).attr('alt') != '')
+        {
+          $(storyImageOverlayCaption).html('');
+          $(storyImageOverlayCaption).css('width', '5px');
+        }
+        else
+        {
+          $(storyImageOverlayCaption).html($(storyImageOverlayImg).attr('alt'));
+          $(storyImageOverlayCaption).css('width', $(storyImageOverlayImg).width());
+        }
+      });
+
     /*update story image overlay caption on image load*/
     $(storyImageOverlayImg).on('load', function()
       {
-        var imgOffset = $(storyImageOverlayImg).offset();
-
-        $(storyImageOverlayCaption).css('left', imgOffset.left);
-        $(storyImageOverlayCaption).css("width", $(storyImageOverlayImg).width());
-
-        $(storyImageOverlayContentDiv).css("width", $(window).width());
-        $(storyImageOverlayContentDiv).css("height", $(window).height());
+        reloadStoryImageOverlayCaption();
       });
   });
