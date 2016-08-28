b5b6eda8f0478057cd975b753e31d2fe4c4e7bd3
ludak <ludak@ludak.me>
2016-08-28 19:14:55 +0000

    Install script installs latest ruby.

--- prerequisites/install.sh
+++ prerequisites/install.sh
@@ -12,28 +12,23 @@ and sudo add-apt-repository "deb http://linux.dropbox.com/ubuntu $UBUNTU_RELEASE
 and sudo apt-get update
 
 # necessary packages
-and sudo apt-get install build-essential ruby2.0 ruby2.0-dev apache2 nodejs libmagickwand-dev dropbox python-gpgme ruby-rspec-core GraphicsMagick
+and sudo apt-get install build-essential ruby ruby-dev apache2 nodejs libmagickwand-dev dropbox python-gpgme ruby-rspec-core graphicsmagick
 
-# use ruby 2.0
-and sudo rm /usr/bin/ruby /usr/bin/irb
-and sudo ln -s /usr/bin/ruby2.0 /usr/bin/ruby
-and sudo ln -s /usr/bin/irb2.0 /usr/bin/irb
-
-# setup gem 2.0
+# setup gems
 and sudo gem install rubygems-update
 and sudo update_rubygems
-and sudo rm /usr/bin/gem
-and sudo ln -s /usr/bin/gem2.0 /usr/bin/gem
-
 and sudo gem install bundler
-and sudo gem update
+and gem update
 
 # build website
-and sudo bundle install
+and cd $LUDAK_HOME/www
+and sudo gem install rmagick
+and bundle install
 and jekyll build
+and cd -
 
 # setup webserver
-and sudo copy $LUDAK_HOME/prerequisites/configuration/ludak.me.conf /etc/apache2/sites-available
+and sudo cp $LUDAK_HOME/prerequisites/configuration/ludak.me.conf /etc/apache2/sites-available
 and sudo rm /etc/apache2/sites-enabled/000-default.conf
 and sudo ln -s /etc/apache2/sites-available/ludak.me.conf /etc/apache2/sites-enabled/ludak.me.conf
 and sudo ln -s $LUDAK_HOME/www/_site /var/www/ludak.me
